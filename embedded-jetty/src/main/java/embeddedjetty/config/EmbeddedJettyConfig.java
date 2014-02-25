dbaf16af66ac3de0082c9e6dde0cfbaec0bd38ae
sameer garg <garg.sameer@gmail.com>
2014-02-25 17:57:04 +0000

    Embedded jetty - WIP

---
+++ embedded-jetty/src/main/java/embeddedjetty/config/EmbeddedJettyConfig.java
@@ -0,0 +1,9 @@
+package embeddedjetty.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "embeddedjetty")
+public class EmbeddedJettyConfig {
+}
---
+++ embedded-jetty/src/main/java/embeddedjetty/controller/HelloWorld.java
@@ -0,0 +1,20 @@
+package embeddedjetty.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorld {
+
+    @Value("${example.message}")
+    private String message;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String showIndex() {
+        return "Hello world! "+message;
+    }
+}
