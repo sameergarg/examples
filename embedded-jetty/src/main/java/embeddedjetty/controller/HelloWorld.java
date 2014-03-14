906ef47a963bb49cc258f58885493849313a6dc2
sameer garg <sameer.garg@which.co.uk>
2014-03-14 10:49:58 +0000

    Added view

--- embedded-jetty/src/main/java/embeddedjetty/controller/HelloWorld.java
+++ embedded-jetty/src/main/java/embeddedjetty/controller/HelloWorld.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorld {
@@ -16,5 +17,11 @@ public class HelloWorld {
     @ResponseBody
     public String showIndex() {
         return "Hello world!! "+message;
+    }
+
+    @RequestMapping(value = "/hello-world", method = RequestMethod.GET)
+    @ResponseBody
+    public ModelAndView helloWorld() {
+        return new ModelAndView("helloWorld");
     }
 }
---
+++ embedded-jetty/src/main/webapp/views/helloWorld.jsp
@@ -0,0 +1,5 @@
+<html>
+    <head></head>
+
+    <body><h1>Hello Sameer!</h1></body>
+</html>
